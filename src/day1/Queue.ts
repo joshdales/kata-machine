f5c60fc8b728fba25af5901109f59d42269dea46
Josh Dales <git@joshdales.dev>
2023-10-15 14:56:38 -0400

    Implment Queue

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,9 +1,53 @@
+interface Node<T> {
+    value: T;
+    next?: Node<T>;
+}
+
 export default class Queue<T> {
     public length: number;
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
-    constructor() {}
+    constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
+    }
 
-    enqueue(item: T): void {}
-    deque(): T | undefined {}
-    peek(): T | undefined {}
+    enqueue(item: T): void {
+        const node = { value: item };
+        this.length++;
+
+        if (!this.tail) {
+            this.head = this.tail = node;
+
+            return;
+        }
+
+        // Setting the next of the current tail
+        this.tail.next = node;
+        // Set the new tail
+        this.tail = node;
+    }
+
+    deque(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
+
+        this.length--;
+        const head = this.head;
+        this.head = this.head.next;
+
+        head.next = undefined;
+
+        if (this.length === 0) {
+            this.tail = undefined;
+        }
+
+        return head.value;
+    }
+
+    peek(): T | undefined {
+        return this.head?.value;
+    }
 }
