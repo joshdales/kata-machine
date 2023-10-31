a3b5852632f65ef7071dc5570fd170619e8777b4
Josh Dales <git@joshdales.dev>
2023-10-31 10:18:19 -0400

    DFS GraphList

--- src/day2/DFSGraphList.ts
+++ src/day2/DFSGraphList.ts
@@ -1,3 +1,51 @@
-export default function dfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
+function walk(
+    graph: WeightedAdjacencyList,
+    curr: number,
+    needle: number,
+    seen: boolean[],
+    path: number[],
+): boolean {
+    if (seen[curr]) {
+        return false;
+    }
 
+    seen[curr] = true;
+
+    // Recursion
+    // pre
+    path.push(curr);
+    if (curr === needle) {
+        return true;
+    }
+
+    // recurse
+    const list = graph[curr];
+    for (let i = 0; i < list.length; ++i) {
+        const edge = list[i];
+        if (walk(graph, edge.to, needle, seen, path)) {
+            return true;
+        }
+    }
+
+    // post
+    path.pop();
+
+    return false;
+}
+
+export default function dfs(
+    graph: WeightedAdjacencyList,
+    source: number,
+    needle: number,
+): number[] | null {
+    const seen = new Array(graph.length).fill(false);
+    const path: number[] = [];
+
+    walk(graph, source, needle, seen, path);
+
+    if (path.length) {
+        return path;
+    }
+
+    return null;
 }
